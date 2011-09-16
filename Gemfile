dbae1adbd7321c7e0843a9009deaa7a936bba757
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2011-09-15 23:18:00 -0300

    Init application, add nifty layout

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.1.rc1'
+gem 'sqlite3'
+gem 'jquery-rails'
+gem 'nifty-generators'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+I18nAlchemyExample::Application.config.secret_token = '9d232074a65350b81560831becec58d5b0bab246cba6ff337aacaa606ee7370f9a6808620d1d071dee250ae0270be804715418f54d3f76b6dafda905e0086e61'
