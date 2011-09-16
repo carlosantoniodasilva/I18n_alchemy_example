fb4d953f388c06bbf32ae543df324e62ad34cf17
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2011-09-16 00:01:35 -0300

    Add change locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+
+  private
+
+  def set_locale
+    if locale = params[:locale] || session[:locale]
+      I18n.locale = session[:locale] = locale
+    end
+  end
 end
